ffa71e0c3477c2e422f5e97dc73931e73a602395
Romeo Namunde <romeomureithi@gmail.com>
2024-12-02 17:31:46 +0300

    transaction page

---
+++ backend/crud.py
@@ -0,0 +1,15 @@
+class CRUD:
+    def __init__(self):
+        pass
+
+    def create():
+        pass
+
+    def read():
+        pass
+
+    def update():
+        pass
+
+    def delete():
+        pass
