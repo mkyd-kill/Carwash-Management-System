a306cbdc5c27dbea56feca55a8e63b502a984699
Romeo Namunde <romeomureithi@gmail.com>
2024-12-08 15:41:39 +0300

    added readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+## 📖 Prerequisites
+
+In order to run the project, we need to have python installed and the required modules
+To install modules, run:
+
+```shell
+pip install -r requirements.txt
+```
+
+## 🖥️ Local Development
+
+To run project server on local machine, run:
+
+```shell
+python main.py
+```
--- backend/models.py
+++ backend/models.py
@@ -1,11 +1,21 @@
-class Clients:
-    pass
+from typing import Optional
+from sqlmodel import SQLModel, Field
+from datetime import datetime
+from shortuuid import ShortUUID
+from string import digits
 
-class Staff:
-    pass
+class BaseModel(SQLModel):
+    id: Optional[int] = Field(default_factory=lambda: ShortUUID(unique=True, max_length=5, alphabet=digits), primary_key=True)
+    date_created: Optional[datetime] = Field(default_factory=lambda: datetime.now())
 
-class Transaction:
-    pass
+class Clients(BaseModel):
+    vehicle: str = Field(nullable=False)
 
-class Report:
-    pass
+class Staff(BaseModel):
+    name: str = Field(nullable=False)
+
+class Transaction(BaseModel):
+    vehicle: str = Field(nullable=False)
+
+class Report(BaseModel):
+    report_type: str = Field(nullable=False)
